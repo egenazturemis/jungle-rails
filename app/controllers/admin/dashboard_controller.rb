ee3db64603a3cd3989a9bbf222ed78e6b160f59e
Egenaz <egenazturemis@gmail.com>
2021-02-05 22:34:45 +0000

    implement a basic admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with :name => ENV["HTTP_AUTH_USERNAME"], :password => ENV["HTTP_AUTH_PASSWORD"]
 
-  def show
-  end
+  def show 
+    @products = Product.count
+    @categories = Category.count
+  end 
 end
